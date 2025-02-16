7032dea0d44b17ee582a26aa6c7efa59bc81a296
euncheol-kim <klmeuncheol@kakao.com>
2025-02-16 18:12:51 +0900

    chore: 모듈 테스트 코드 삭제 및 JPA 종속성 제거

--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/repository/ExampleRepository.java
+++
@@ -1,6 +0,0 @@
-package com.culturespot.culturespotdomain.repository;
-
-import com.culturespot.culturespotdomain.entity.ExampleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ExampleRepository extends JpaRepository<ExampleEntity, Long> {}

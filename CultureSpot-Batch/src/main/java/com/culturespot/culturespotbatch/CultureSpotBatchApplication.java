203f55cd7791e302f8dc6afba40479421cbc7fe9
jwj <dnwls16071@naver.com>
2025-03-06 00:25:51 +0900

    [chore⚙️] 멀티 모듈 컴포넌트 스캔 추가(#11)

--- CultureSpot-Batch/src/main/java/com/culturespot/culturespotbatch/CultureSpotBatchApplication.java
+++ CultureSpot-Batch/src/main/java/com/culturespot/culturespotbatch/CultureSpotBatchApplication.java
@@ -2,12 +2,20 @@ package com.culturespot.culturespotbatch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"com.culturespot.culturespotbatch",
+		"com.culturespot.culturespotapi",
+		"com.culturespot.culturespotdomain",
+		"com.culturespot.culturespotcommon"
+})
 public class CultureSpotBatchApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CultureSpotBatchApplication.class, args);
 	}
-
 }

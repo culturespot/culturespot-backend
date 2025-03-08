e81878d28f84fad5ad7f9351827a7060d0fcaee1
euncheol-kim <klmeuncheol@kakao.com>
2025-03-08 12:04:01 +0900

    [feat✨] CultureSpotDomain: Community/ImageService 이미지 생성, 삭제, 저장 기능 추가 (#15)

---
+++ CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/community/service/ImageService.java
@@ -0,0 +1,13 @@
+package com.culturespot.culturespotdomain.core.community.service;
+
+import com.culturespot.culturespotdomain.core.community.entity.Image;
+import com.culturespot.culturespotdomain.core.community.entity.Post;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+public interface ImageService {
+    List<Image> createImage(Post post, List<MultipartFile> files);
+    void deleteImages(Long id);
+    void deleteImages(List<Long> id);
+}

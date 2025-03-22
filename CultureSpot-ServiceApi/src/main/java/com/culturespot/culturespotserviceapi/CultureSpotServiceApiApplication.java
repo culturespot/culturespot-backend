c0d71b7f3a684bcfbba6d341af5905e20e456604
euncheol-kim <klmeuncheol@kakao.com>
2025-03-22 14:26:20 +0900

    [feat✨] ServiceApi module - @Import문에 Domain모듈의 엔티티 및 레포지토리를 읽을 수 있도록 JpaConfig 추가

--- CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/CultureSpotServiceApiApplication.java
+++ CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/CultureSpotServiceApiApplication.java
@@ -1,16 +1,16 @@
 package com.culturespot.culturespotserviceapi;
 
 import com.culturespot.culturespotdomain.core.global.config.DomainModuleConfig;
+import com.culturespot.culturespotdomain.core.global.config.JpaConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Import;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EntityScan(basePackages = "com.culturespot.culturespotdomain.core")
-@EnableJpaRepositories(basePackages = "com.culturespot.culturespotdomain.core")
-@Import(DomainModuleConfig.class)
+@Import({
+		DomainModuleConfig.class,
+		JpaConfig.class
+})
 public class CultureSpotServiceApiApplication {
 
 	public static void main(String[] args) {

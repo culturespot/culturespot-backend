213aa1cd61bce95e125165c5ec4c0545e4e9b016
euncheol-kim <klmeuncheol@kakao.com>
2025-04-04 15:56:15 +0900

    [feat✨] 주석 수정 및 User객체 생성 추상 메소드 선언

--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/user/service/UserService.java
+++ CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/user/service/UserService.java
@@ -18,27 +18,16 @@ public interface UserService{
      * @return 사용자가 가진 권한 이름의 집합 (예: "USER", "ADMIN")
      * */
     Set<String> getRoleNames(User user);
-
     /**
      * 주어진 이메일에 해당하는 사용자를 조회
      * <p>
      * 사용자가 존재하면 해당 {@link User} 객체를 반환하고, 존재하지 않으면 {@link AuthException}을 발생시킵니다.
      * </p>
      *
-     * @param email 사용자의 이메일 주소
-     * @return 이메일에 해당하는 {@link User} 객체 반환
      * @throws {@link AuthException} 사용자가 존재하지 않는다면 {@link AuthExceptionCode#USER_NOT_FOUND} 예외를 발생
      */
     User findUserOrThrow(String email);
-
-    /**
-     * 사용자가 존재하지 않으면 새로 생성하고 저장하는 메서드
-     *
-     * @param email 사용자의 이메일
-     * @param authType 소셜 로그인 타입 (예: {@link SocialLoginType#GOOGLE}, {@link SocialLoginType#KAKAO})
-     * @return 생성되거나 기존에 존재하던 {@link User} 객체
-     */
     User createUserIfNotExists(String email, SocialLoginType authType);
-
-    public void updateLastLoginAt(User user);
+    User createUser(String email, SocialLoginType authType);
+    void updateLastLoginAt(User user);
 }

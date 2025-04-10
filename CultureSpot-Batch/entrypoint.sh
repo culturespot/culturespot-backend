f44687970e4951669d1dbe033b64ee829580853c
spearkkk <spearkkp@gmail.com>
2025-04-11 02:11:49 +0900

    [CUL-127] Change deployment for batch

--- CultureSpot-Batch/entrypoint.sh
+++ CultureSpot-Batch/entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
-echo "SPRING_PROFILE: ${SPRING_PROFILE}"
-exec java -Dspring.profiles.active=${SPRING_PROFILE} -jar /app/batch.jar
+JVM_OPTIONS=${JVM_OPTIONS:-""}
+
+echo "Executing: java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule=${CRON_SCHEDULE} -jar /app/batch.jar"
+exec java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule=${CRON_SCHEDULE} -jar /app/batch.jar
--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/common/DiscordMessageServiceMock.java
+++ CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/common/DiscordMessageServiceMock.java
@@ -1,7 +1,6 @@
 package com.culturespot.culturespotdomain.common;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
@@ -10,12 +9,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class DiscordMessageServiceMock implements DiscordMessageService {
 
-  @Value("${discord.webhook}")
-  private String webHook;
-
   @Override
   public void send(String title, String description, Color color) {
-    log.info("webhook: {}", webHook);
     log.info("title: {}, description: {}, color: {}", title, description, color);
     log.info("Completed to send Discord message.");
   }

03850da7eebde34a01cc13b9ea05dc18c4c63845
spearkkk <spearkkp@gmail.com>
2025-04-11 02:24:01 +0900

    [CUL-127] Fix bug

--- CultureSpot-Batch/entrypoint.sh
+++ CultureSpot-Batch/entrypoint.sh
@@ -2,5 +2,5 @@
 
 JVM_OPTIONS=${JVM_OPTIONS:-""}
 
-echo "Executing: java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule=${CRON_SCHEDULE} -jar /app/batch.jar"
-exec java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule=${CRON_SCHEDULE} -jar /app/batch.jar
+echo "Executing: java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule=\"${CRON_SCHEDULE}\" -jar /app/batch.jar"
+exec java ${JVM_OPTIONS} -Dspring.profiles.active=${SPRING_PROFILE} -Dperformance.migration.job.cron.schedule="${CRON_SCHEDULE}" -jar /app/batch.jar

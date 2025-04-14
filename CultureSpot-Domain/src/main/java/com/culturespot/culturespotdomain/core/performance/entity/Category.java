78ef4a56170253c63fd9b8ffc4a4c32683bb2d1f
euncheol-kim <klmeuncheol@kakao.com>
2025-04-14 20:14:01 +0900

    [feat] 카테고리를 문자열 요청으로 받았을 때 name값과 비교하여 Category 반환하는 메소드 추가  (CUL-173)

--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/performance/entity/Category.java
+++ CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/performance/entity/Category.java
@@ -1,5 +1,12 @@
 package com.culturespot.culturespotdomain.core.performance.entity;
 
+import lombok.Getter;
+
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Getter
 public enum Category {
 
   EXHIBITION("전시"),
@@ -28,4 +35,15 @@ public enum Category {
     }
     return ETC;
   }
+
+  public static Category fromString(String value) {
+    return Arrays.stream(Category.values())
+            .filter(category -> category.getName().equals(value))
+            .findFirst()
+            .orElse(Category.ETC);
+  }
+
+  private static final Set<String> VALID_NAMES = Arrays.stream(values())
+          .map(Category::getName)
+          .collect(Collectors.toSet());
 }

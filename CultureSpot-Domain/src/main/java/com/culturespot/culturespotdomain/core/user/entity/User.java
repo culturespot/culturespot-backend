4e7b7cd8c7914005830fd5640baf39a2d36e3ec7
euncheol-kim <klmeuncheol@kakao.com>
2025-04-14 19:57:08 +0900

    [feat] User 엔티티 수정 (CUL-173)
    # 대표 수정내역
    # 1. password 필드 삭제 : 현재 서비스는 소셜 로그인만 지원하기 때문에 삭제 진행
    # 2. preferredCategory 필드 추가 : 사용자의 선호하는 카테고리
    # 3. updatePreferredCategory(List<String> categories) : 필드 상태 변경을 위한 도메인 메소드 생성

--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/user/entity/User.java
+++ CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/user/entity/User.java
@@ -1,15 +1,17 @@
 package com.culturespot.culturespotdomain.core.user.entity;
 
 import com.culturespot.culturespotdomain.core.global.entity.BaseEntity;
+import com.culturespot.culturespotdomain.core.global.exception.DomainException;
+import com.culturespot.culturespotdomain.core.global.exception.DomainExceptionCode;
+import com.culturespot.culturespotdomain.core.performance.entity.Category;
 import jakarta.persistence.*;
-import lombok.AccessLevel;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
+
+import java.util.*;
 
 @Entity
 @Table(name = "users")
@@ -24,12 +26,11 @@ public class User extends BaseEntity {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
-    private String password;
-
+    @Setter
     @Column(nullable = false, unique = true)
     private String nickname;
 
+    @Setter
     @Column(name="profile_code", nullable = false)
     private int profileCode;
 
@@ -37,11 +38,15 @@ public class User extends BaseEntity {
     @Column(nullable = false)
     private SocialLoginType authType;
 
+    @JdbcTypeCode(SqlTypes.JSON)
+    @Column(name="preferred_category", columnDefinition="json")
+    Map<Category, String> preferredCategory = new HashMap<>();
+
     @Column(name="last_login_at", nullable = false)
     private LocalDateTime lastLoginAt;
     // ************************ column ************************ //
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY, cascade = CascadeType.ALL, orphanRemoval = true)
     private Set<UserRole> roles = new HashSet<>();
 
     @Builder
@@ -50,21 +55,29 @@ public class User extends BaseEntity {
             String nickname,
             SocialLoginType authType,
             Set<UserRole> roles,
-            String password,
             int profileCode,
-            LocalDateTime lastLoginAt
+            LocalDateTime lastLoginAt,
+            Map<Category, String> preferredCategory
     ){
         this.email = email;
         this.nickname = nickname;
         this.authType = authType;
-        this.roles = roles;
-        this.password = password;
+        this.roles = roles != null ? roles : new HashSet<>();
         this.profileCode = profileCode;
         this.lastLoginAt = lastLoginAt;
+        this.preferredCategory = preferredCategory != null ? preferredCategory : new HashMap<>();
     }
 
     public void addRole(UserRole userRole) {
         this.roles.add(userRole);
         userRole.setUser(this);
+    }
+
+    public void updatePreferredCategory(List<String> categories) {
+        preferredCategory.clear();
+        for (String categoryName : categories) {
+            Category category = Category.fromString(categoryName);
+            preferredCategory.put(category, category.getName());
+        }
     }
 }

f70703df29158f87347db49614362eee70f3682f
euncheol-kim <klmeuncheol@kakao.com>
2025-04-15 17:09:19 +0900

    [refactor] NamedWrapper 제네릭 추가(CUL-173)
    # 리팩토링 의의
    # - Swagger에서 응답값을 만들 때 범용성 있게 사용하기 위함
    # - 대표 적용 케이스 : SwaggerUserProfileResponse

--- CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/global/utils/response/NamedWrapper.java
+++ CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/global/utils/response/NamedWrapper.java
@@ -3,15 +3,16 @@ package com.culturespot.culturespotserviceapi.core.global.utils.response;
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
 import java.util.Map;
 
-public class NamedWrapper extends ApiResponse{
-    private final Map<String, Object> content;
+public class NamedWrapper<T> extends ApiResponse {
 
-    public NamedWrapper(String key, Object value) {
+    private final Map<String, T> content;
+
+    public NamedWrapper(String key, T value) {
         this.content = Map.of(key, value);
     }
 
     @JsonAnyGetter
-    public Map<String, Object> getContent() {
+    public Map<String, T> getContent() {
         return content;
     }
 }

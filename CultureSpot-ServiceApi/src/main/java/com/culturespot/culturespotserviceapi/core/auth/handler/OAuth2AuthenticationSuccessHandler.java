2a2def7bbe431a2fafb47d609dccac9dc7d8e541
euncheol-kim <klmeuncheol@kakao.com>
2025-04-20 17:40:15 +0900

    [refactor] 임시 리다이렉트 URI 설정 (로그인 성공시)
    # 차후 수정필요

--- CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/auth/handler/OAuth2AuthenticationSuccessHandler.java
+++ CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/auth/handler/OAuth2AuthenticationSuccessHandler.java
@@ -66,15 +66,15 @@ public class OAuth2AuthenticationSuccessHandler extends SimpleUrlAuthenticationS
         // ✅ refreshToken db 저장 (SocialLoginType 포함)
         refreshTokenService.saveRefreshToken(email,  SocialLoginType.fromRegistrationId(registrationId), refreshToken);
 
-        // ✅ JSON 응답 설정
-        response.setStatus(HttpServletResponse.SC_OK);
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
+//        // ✅ JSON 응답 설정
+//        response.setStatus(HttpServletResponse.SC_OK);
+//        response.setContentType("application/json");
+//        response.setCharacterEncoding("UTF-8");
+//
+//        // ✅ 응답 객체 생성 & 사용자 최신 로그인 시간 업데이트
+//        LoginSuccessResponse responseDto = oAuth2LoginSuccessHandler.handle(registrationId, email);
 
-        // ✅ 응답 객체 생성 & 사용자 최신 로그인 시간 업데이트
-        LoginSuccessResponse responseDto = oAuth2LoginSuccessHandler.handle(registrationId, email);
-
-        new ObjectMapper()
-                .writeValue(response.getWriter(), responseDto);
+        // ✅ 리디렉트 (프론트 페이지로 이동)
+        response.sendRedirect("http://localhost:3000");
     }
 }

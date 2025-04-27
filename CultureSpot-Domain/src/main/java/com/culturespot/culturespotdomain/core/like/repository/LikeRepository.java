c2fbfc106f05987278d034955edac2e206e6b53d
euncheol-kim <klmeuncheol@kakao.com>
2025-04-27 10:36:30 +0900

    [remove] 커뮤니티와 관련한 기존 파일 삭제
    # 삭제의의:
    # - 작업자 김은철이 모두 작성했으며 개발 단계에 있던 임시 코드였습니다.
    # - aggregate 원칙을 의거하여 커뮤니티 패키지를 응집도를 높이고 명확한 네이밍을 적용하기 위해 삭제합니다.

--- CultureSpot-Domain/src/main/java/com/culturespot/culturespotdomain/core/like/repository/LikeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.culturespot.culturespotdomain.core.like.repository;
-
-import com.culturespot.culturespotdomain.core.like.entity.Like;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LikeRepository extends JpaRepository<Like, Long> {
-}

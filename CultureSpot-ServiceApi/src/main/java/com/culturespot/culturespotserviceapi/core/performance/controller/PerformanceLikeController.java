2036055ed8c810d127d637afd2cbed51d61b6fe5
Chaewon <lcw3714@naver.com>
2025-05-04 12:51:35 +0900

    [fix] 이벤트 좋아요 MemberOnly 어노테이션 수정(CUL-268)

--- CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/performance/controller/PerformanceLikeController.java
+++ CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/performance/controller/PerformanceLikeController.java
@@ -3,10 +3,10 @@ package com.culturespot.culturespotserviceapi.core.performance.controller;
 import com.culturespot.culturespotdomain.core.user.entity.User;
 import com.culturespot.culturespotserviceapi.common.security.endpoint.EndpointType;
 import com.culturespot.culturespotserviceapi.core.auth.annotation.Auth;
-import com.culturespot.culturespotserviceapi.core.auth.annotation.MemberOnly;
 import com.culturespot.culturespotserviceapi.core.performance.service.PerformanceLikeService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -18,14 +18,14 @@ public class PerformanceLikeController {
 
     private final PerformanceLikeService likeService;
 
-    @MemberOnly
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @PostMapping("/{eventId}/like")
     public ResponseEntity<?> like(@Auth User user, @PathVariable Long eventId){
         likeService.like(user, eventId);
         return ResponseEntity.ok(Map.of("liked", true));
     }
 
-    @MemberOnly
+    @PreAuthorize("hasAuthority('ROLE_USER')")
     @DeleteMapping("/{eventId}/like")
     public ResponseEntity<?> unlike(@Auth User user, @PathVariable Long eventId){
         likeService.unlike(user, eventId);

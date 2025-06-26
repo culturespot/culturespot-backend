3a22eb64ae84440a232905b932b83ef54148a1e4
Chaewon <lcw3714@naver.com>
2025-06-26 21:02:19 +0900

    [docs] 이벤트 목록 조회 스웨거 추가(CUL-281)

--- CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/performance/controller/PerformanceController.java
+++ CultureSpot-ServiceApi/src/main/java/com/culturespot/culturespotserviceapi/core/performance/controller/PerformanceController.java
@@ -6,11 +6,13 @@ import com.culturespot.culturespotdomain.core.performance.entity.Sort;
 import com.culturespot.culturespotdomain.core.user.entity.User;
 import com.culturespot.culturespotserviceapi.common.security.endpoint.EndpointType;
 import com.culturespot.culturespotserviceapi.core.auth.annotation.Auth;
-import com.culturespot.culturespotserviceapi.core.performance.controller.spec.PerformanceControllerSpec;
 import com.culturespot.culturespotserviceapi.core.performance.dto.response.PerformanceDetailResponse;
 import com.culturespot.culturespotserviceapi.core.performance.dto.response.PerformanceListResponse;
 import com.culturespot.culturespotserviceapi.core.performance.dto.response.PerformanceListSimpleResponse;
 import com.culturespot.culturespotserviceapi.core.performance.service.PerformanceService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,17 +20,19 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "이벤트 API")
 @RestController
 @RequiredArgsConstructor
-public class PerformanceController implements PerformanceControllerSpec {
+public class PerformanceController{
 
     private final PerformanceService performanceService;
 
+    @Operation(summary = "이벤트 목록 조회")
     @GetMapping("/api/events")
     public PerformanceListResponse getPerformances(
             @Auth User user,
-            @RequestParam(required = false) Event event,
-            @RequestParam(required = false) Category category,
+            @Parameter(description = "이벤트 대분류") @RequestParam(required = false) Event event,
+            @Parameter(description = "이벤트 중분류") @RequestParam(required = false) Category category,
             @RequestParam(required = false) Sort sort,
             @RequestParam int size,
             @RequestParam(required = false) Long lastId,
@@ -37,6 +41,7 @@ public class PerformanceController implements PerformanceControllerSpec {
         return performanceService.getPerformances(user, event, category, sort, size, lastId, keyword);
     }
 
+    @Operation(summary = "실시간 인기 이벤트 조회")
     @GetMapping("/api/events/popular")
     public PerformanceListSimpleResponse getPopularPerformances(
             @Auth User user,
@@ -45,6 +50,7 @@ public class PerformanceController implements PerformanceControllerSpec {
         return performanceService.getPopularPerformances(user, event);
     }
 
+    @Operation(summary = "사용자 맞춤 이벤트 조회")
     @PreAuthorize("hasAuthority('ROLE_USER')")
     @GetMapping(EndpointType.USER_PATH + "/events/recommended")
     public PerformanceListSimpleResponse getRecommendedPerformances(
@@ -54,6 +60,7 @@ public class PerformanceController implements PerformanceControllerSpec {
         return performanceService.getRecommendedPerformances(user, event);
     }
 
+    @Operation(summary = "이벤트 상세 정보 조회")
     @GetMapping("/api/events/{eventId}")
     public PerformanceDetailResponse getPerformanceDetail(
             @Auth User user,
